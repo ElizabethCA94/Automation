a1eb864b4414b83747f2c55c9d44e8a006c2a446
ElizabethCA94 <ecarreno@unal.edu.co>
2022-07-26 21:53:54 -0500

    test: portal members login auth

---
+++ cypress/e2e/members-portal/login.cy.js
@@ -0,0 +1,32 @@
+describe('members portal', () => {
+  before(() => {
+    cy.visit('https://ns-booking-dev.newshore.es/en-US/')
+  })
+
+  it('login to members portal', () => {
+    cy.intercept('https://ns-booking-dev.newshore.es/en-us/members/profile/')
+      .as('profilePageLoad')
+
+    cy.get('.main-header_nav-secondary_list').then(() => {
+      cy.get('.auth').click()
+    })
+
+    cy.get('.auth-popover_login').then(() => {
+      cy.get('#emailLoginInput')
+        .click()
+        .type('paula@g.com')
+
+      cy.get('#passwordLoginInput')
+        .click()
+        .type('Newshore2020*')
+      
+      cy.get('.ui-form_button-action.button').click()
+    })
+
+    cy.wait('@profilePageLoad')
+
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('https://ns-booking-dev.newshore.es/en-us/members/profile/')
+    })
+  })
+})
